9975890d4a4edb949e47358c3428b07cf54b6815
WarinCode <warinsaipanya471@outlook.co.th>
2025-12-05 10:37:02 +0700

    Add package-lock.json and update package.json dependencies

--- week2/src/index.js
+++ week2/src/index.js
@@ -1,10 +1,20 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import logger from "morgan";
 
 const app = express();
 const port = 3000;
 
 app
+  .use(json())
+  .use(urlencoded({ extended: true }))
+  .use(logger("dev"));
+
+app
   .get("/", (req, res) => res.send("Hello World!"))
+  .get("/name/:name", ({ params: { name } }, res) =>
+    res.send(`Hello, ${name}!`)
+  )
+  .post("/data", (req, res) => res.send(req.body))
   .listen(port, () =>
     console.log(`Server is running on http://localhost:${port}`)
   );
